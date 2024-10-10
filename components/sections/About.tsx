83974fb3b8e9f985858a5c72ec0d9d2c9ec71be9
Francis <francis.i.authority@gmail.com>
2024-10-10 12:37:17 +0100

    about section in progress.

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Globe from 'react-globe.gl';
+import Button from '../ui/Button';
 
 const About = () => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('francis.i.authority@gmail.com');
+
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  }
   return (
     <section className='c-space my-20' id='about'>
       <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
@@ -11,7 +22,7 @@ const About = () => {
             <Image 
               src="/assets/grid1.svg" 
               alt='grid-1' 
-              width={10} height={10}
+              width={1} height={1}
               className='w-full sm:h-[276px] h-fit object-contain'
             />
             <p className='grid-headtext'>
@@ -22,13 +33,12 @@ const About = () => {
             </p>
           </div>
         </div>
-
         <div className='col-span-1 xl:row-span-3'>
           <div className='grid-container'>
             <Image 
               src="/assets/grid2.svg" 
               alt='grid-1' 
-              width={10} height={10}
+              width={1} height={1}
               className='w-full sm:h-[276px] h-fit object-contain'/>
               <div>
                 <p className='grid-headtext'>Tech Stack</p>
@@ -36,12 +46,82 @@ const About = () => {
               </div>
           </div>
         </div>
-
         <div className='col-span-1 xl:row-span-4'>
-          <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-            <Globe
-              height={326}
+          <div className='grid-container'>
+            <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor='rgba(0,0,0,0)'
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+              />
+            </div>
+            <div>
+              <p className='grid-headtext'>
+              I’m very flexible with time zone communications & locations
+              </p>
+              <p className='grid-subtext'>
+                I&apos;m based in Nigeria, with remote work available.
+              </p>
+              <Button 
+                name="Contact Me" 
+                isBeam 
+                containerClass="w-full mt-10"
+              />
+            </div>
+          </div>
+        </div>
+        <div className='xl:col-span-2 xl:row-span-3'>
+          <div className='grid-container'>
+            <Image 
+              src={'/assets/grid3.svg'} 
+              alt='grid-3'
+              width={1}
+              height={1}
+              className='w-full sm:h-[266px] h-fit object-contain'
             />
+            <div>
+              <p className='grid-headtext'>
+                My Passion for Coding
+              </p>
+              <p className='grid-subtext'>
+                I love solving problems and building things through code. Programming isn&apos;t just my profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+          </div>
+        <div className='xl:col-span-1 xl:row-span-2'>
+          <div className='grid-container'>
+            <Image
+              src={'/assets/grid4.svg'}
+              alt='grid-4'
+              width={1}
+              height={1}
+              className='w-full md:h-[126px] s,:h-[276px] h-fit object-cover sm:object-top'
+            />
+            <div className='space-y-2'>
+              <p className='grid-subtext text-center'>
+                Contact me
+              </p>
+              <div className='copy-container' onClick={handleCopy}>
+                <Image
+                  src={isCopied 
+                    ? 'assets/tick.svg' 
+                    : 'assets/copy.svg'
+                  }
+                  alt='copy'
+                  width={1}
+                  height={1}
+                  className='w-8 h-8'                
+                />
+                <p className='grid-subtext lg:text-xl md:text-lg font-medium text-gray_medium '>
+                  francis.i.authority@gmail.com
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     <header className='fixed py-1 top-0 right-0 left-0 z-50 bg-black/90'>
       <div className='max-w-7xl mx-auto'>
         <div className='flex justify-between items-center py-5 mx-auto c-space'>
-          <Link href="/" className='text-neutral-300 font-bold text-3xl hover:text-white transition-colors'>
+          <Link href="/" className='text-neutral-400 font-bold text-3xl hover:text-white transition-colors'>
             FA
           </Link>
 
